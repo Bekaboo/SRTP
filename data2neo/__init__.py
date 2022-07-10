51c5259574c5e7117120edfad564370574bb69c7
bekaboo <18127878294@qq.com>
2022-07-10 22:33:40 +0800

    Delete list2neo; Add support for multiple properties & labels for file2neo.

--- data2neo/__init__.py
+++ data2neo/__init__.py
@@ -1,2 +1,1 @@
 from .file2neo import File2Neo as FromFile
-from .list2neo import List2Neo as FromList
--- data2neo/file2neo.py
+++ data2neo/file2neo.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#
 import pandas as pd
-import py2neo as neo
-
+import py2neo as pn
+import os.path as path
+import re
 
 # This class is used to build a link between our datafile and the remote graph,
 # subsequently create the nodes and relationships from the datafile.
+
+
 class File2Neo(object):
     def __init__(self, filepath, graph_link="http://localhost:7474/",
                  graph_auth=("neo4j", "neo4j")):
 
         # Link to the remote graph.
-        graph = neo.Graph(graph_link, auth=graph_auth)
+        graph = pn.Graph(graph_link, auth=graph_auth)
         self.graph = graph
         self.filepath = filepath
-        try:
+        datapath_split = path.splitext(filepath)
+        if datapath_split[1] == ".xlsx" or datapath_split[1] == ".xls":
             self.data = pd.read_excel(filepath)
-        except ValueError:
-            self.data = pd.read_csv(filepath, encoding="GB2312")
+        elif datapath_split[1] == ".csv":
+            self.data = pd.read_csv(filepath)
+
+    def _parse_node_labels(self, idx):
+        return str(self.data.node_labels.loc[idx]).strip().replace(";", ":")
+
+    def _parse_node_props(self, idx):
+        return str(self.data.node_props.loc[idx]).strip().replace(";", ",")
+
+    def _parse_relations(self, idx):
+        if pd.isna(self.data.relations.loc[idx]):
+            return
+        relations = [re.split("->", _)
+                     for _ in self.data.relations.loc[idx].strip().split("|")]
+        relation_types = [_[0].split(";") for _ in relations]
+        related_to = [_[1].split("@") for _ in relations]
+        related_to = [[_[0].replace(";", ","), _[1].replace(";", ":")]
+                      for _ in related_to]
+        return {"types": relation_types, "to": related_to}
 
     def file2nodes(self):   # To create a graph, first create all the nodes.
         data = self.data
         graph = self.graph
 
         for i in range(data.index.stop):
-            if not isinstance(data.nodes.loc[i], str):  # Eliminate `nan`.
+            if not isinstance(data.node_props.loc[i], str):  # Eliminate `nan`.
                 continue
-            clause = "MERGE (:%s {name:'%s'})"\
-                % (str(data.labels.loc[i]).strip(),
-                   str(data.nodes.loc[i]).strip())
+            clause = "MERGE (:%s {%s})"\
+                % (self._parse_node_labels(i),
+                   self._parse_node_props(i))
+            print(clause)
             clause = clause.replace("'nan'", "NULL")
             graph.run(clause)
 
@@ -40,36 +61,21 @@ class File2Neo(object):
 
         # Deal with one main node for each loop.
         for i in range(data.index.stop):
-
-            relations = data.relations.loc[i]
-            if not isinstance(relations, str):  # Eliminate `nan`.
+            relations = self._parse_relations(i)
+            if not relations:  # Continue if no relations are attached
                 continue
-            relations = relations.split("|")
-
-            main_node = data.nodes.loc[i].strip()
-            main_node_label = data.labels.loc[i].strip()
-
-            related_nodes = []      # The nodes that related to the main node.
-            related_node_labels = []    # The labels of the related nodes.
-            # The types of the relationships.
-            # `related_nodes` and `relation_types` should have the same length.
-            relation_types = []
-            for relation in relations:
-                relation = relation.strip()
-                rel_list = relation.split()
-                if len(rel_list) == 2:
-                    related_nodes.append(rel_list[0])
-                    related_node_labels.append(rel_list[1])
-                    relation_types.append("CONTAINS")
-                elif len(rel_list) == 3:
-                    related_nodes.append(relation.split()[0])
-                    related_node_labels.append(rel_list[1])
-                    relation_types.append(relation.split()[2])
-
-            # Deal with one related node for each loop.
-            for j in range(len(related_nodes)):
-                clause = "MATCH (main_node:%s {name:'%s'}), (related_node:%s {name:'%s'}) MERGE (main_node)-[:%s]->(related_node)"\
-                    % (main_node_label, main_node, related_node_labels[j],
-                       related_nodes[j], relation_types[j])
-                print(clause)
-                graph.run(clause)
+            for relation in zip(relations.get("types"), relations.get("to")):
+                for relation_type in relation[0]:
+                    related_to_node_props = relation[1][0]
+                    if len(relation[1]) >= 1:
+                        related_to_node_labels = relation[1][1]
+                    clause = "MATCH (main_node:%s {%s}) "\
+                             "WITH main_node MATCH (related_to:%s {%s}) "\
+                             "MERGE (main_node)-[:%s]->(related_to)"\
+                        % (self._parse_node_labels(i),
+                           self._parse_node_props(i),
+                           related_to_node_labels,
+                           related_to_node_props,
+                           relation_type)
+                    print(clause)
+                    graph.run(clause)
